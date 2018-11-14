a5dd38772c8002203230c3918466e1dd334ae8e8
Ben Whitley <ben@touchlab.co>
2018-11-14 16:20:08 -0500

    updated json and coloring changes

--- sessionize/lib/src/commonMain/kotlin/co/touchlab/sessionize/Config.kt
+++ sessionize/lib/src/commonMain/kotlin/co/touchlab/sessionize/Config.kt
@@ -3,4 +3,4 @@ package co.touchlab.sessionize
 import co.touchlab.stately.annotation.SharedImmutable
 
 @SharedImmutable
-const val SESSIONIZE_INSTANCE_ID = "tovwb4kd"
+const val SESSIONIZE_INSTANCE_ID = "4q6ac2c0"
