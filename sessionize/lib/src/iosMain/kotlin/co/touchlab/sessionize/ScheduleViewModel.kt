4c43e147cee83e8c3e4753725fbbe208001c4f55
Kevin Galligan <kgalligan@gmail.com>
2019-02-06 11:30:07 -0500

    New stuff

--- sessionize/lib/src/iosMain/kotlin/co/touchlab/sessionize/ScheduleViewModel.kt
+++ sessionize/lib/src/iosMain/kotlin/co/touchlab/sessionize/ScheduleViewModel.kt
@@ -1,8 +1,19 @@
 package co.touchlab.sessionize
 
 import co.touchlab.sessionize.display.DaySchedule
+import co.touchlab.sessionize.platform.currentTimeMillis
 
 class ScheduleViewModel(allEvents: Boolean){
+    init {
+        xcodeKotlinisActive()
+        if(currentTimeMillis() < 1234) {
+            xcodeKotlinClassName()
+            xcodeKotlinDisposeString()
+            xcodeKotlinArrayType()
+            xcodeKotlinArrayBase()
+            xcodeKotlinArrayTypeSize()
+        }
+    }
     val scheduleModel = ScheduleModel(allEvents)
 
     fun registerForChanges(proc:(notes:List<DaySchedule>)->Unit){
@@ -18,3 +29,27 @@ class ScheduleViewModel(allEvents: Boolean){
         scheduleModel.shutDown()
     }
 }
+
+fun classname(a:Any):String?{
+    return a::class.qualifiedName
+}
+
+@SymbolName("XcodeKotlin_isActive")
+external fun xcodeKotlinisActive():Int
+
+@SymbolName("XcodeKotlin_className")
+external fun xcodeKotlinClassName()
+
+@SymbolName("XcodeKotlin_disposeString")
+external fun xcodeKotlinDisposeString()
+
+@SymbolName("XcodeKotlin_arrayType")
+external fun xcodeKotlinArrayType()
+
+@SymbolName("XcodeKotlin_arrayBase")
+external fun xcodeKotlinArrayBase()
+
+@SymbolName("XcodeKotlin_arrayTypeSize")
+external fun xcodeKotlinArrayTypeSize()
+
+
