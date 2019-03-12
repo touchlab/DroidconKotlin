6b175cea2b990f2183febd2d474d52d180154a5b
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2019-03-12 14:15:13 -0400

    Updated script to change dates on sessions

--- updateDates.py
+++ updateDates.py
@@ -24,11 +24,58 @@ with open("schedule.json", "r") as f:
 # Update date
 todayDateTime = datetime.date.today()
 tomorrowDateTime = datetime.date.today() + datetime.timedelta(days=1)
-date1Str = todayDateTime.strftime("%Y-%m-%dT00:00:00")
-date2Str =  tomorrowDateTime.strftime("%Y-%m-%dT00:00:00")
+date1Str = todayDateTime.strftime("%Y-%m-%d")
+date2Str =  tomorrowDateTime.strftime("%Y-%m-%d")
 
-data[0]["date"] = date1Str
-data[1]["date"] = date2Str
+
+
+# Updating day 1 Session Dates
+for i in range(len(data)):
+    day = data[i]
+
+    dateStr = date1Str
+    if i > 0:
+        dateStr = date2Str
+
+    data[i]["date"] = dateStr + "T" + "00:00:00"
+
+    # Updating Session Dates
+    for room in day["rooms"]:
+        for session in room["sessions"]:
+
+            startingTime = session["startsAt"]
+            oldStartingTime = startingTime.split('T')
+            newStartTime = dateStr + 'T' + oldStartingTime[1]
+            session["startsAt"] = newStartTime
+
+            endingTime = session["endsAt"]
+            oldEndingTime = endingTime.split('T')
+            newEndTime = dateStr + 'T' + oldEndingTime[1]
+            session["endsAt"] = newEndTime
+
+
+
+    #Updating timeslot dates
+    for timeSlots in day["timeSlots"]:
+        for room in timeSlots["rooms"]:
+            session = room['session']
+            if 'startsAt' in session:
+
+                startingTime = session["startsAt"]
+                oldStartingTime = startingTime.split('T')
+                newStartTime = dateStr + 'T' + oldStartingTime[1]
+                session["startsAt"] = newStartTime
+
+                endingTime = session["endsAt"]
+                oldEndingTime = endingTime.split('T')
+                newEndTime = dateStr + 'T' + oldEndingTime[1]
+                session["endsAt"] = newEndTime
+
+            room['session'] = session
+
+    data[i] = day
+
+
 
 # update existing file
 with open("schedule.json", "w") as jsonFile:
