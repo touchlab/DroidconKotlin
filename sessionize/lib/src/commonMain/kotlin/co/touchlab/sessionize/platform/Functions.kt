5b298750dfdc8538e218ff941c4639d2d74af5fd
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2019-03-26 15:52:37 -0400

    moved notifications to their own files

--- sessionize/lib/src/commonMain/kotlin/co/touchlab/sessionize/platform/Functions.kt
+++ sessionize/lib/src/commonMain/kotlin/co/touchlab/sessionize/platform/Functions.kt
@@ -51,13 +51,3 @@ expect fun settingsFactory(): Settings.Factory
  * Generates a unique string for use in tracking this user anonymously
  */
 expect fun createUuid(): String
-
-expect fun createLocalNotification(title:String, message:String, timeInMS:Long, notificationId: Int)
-
-expect fun cancelLocalNotification(notificationId: Int)
-
-expect fun initializeNotifications()
-
-expect fun deinitializeNotifications()
-
-val tenMinutesInMS:Int = 1000 * 10 * 60
