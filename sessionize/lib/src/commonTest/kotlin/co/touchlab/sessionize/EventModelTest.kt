ff8ad33c42b95b5b7f4e62ebc4dbed0256b24e5a
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2019-04-02 11:04:20 -0400

    Added mock Notifications Api for testing

--- sessionize/lib/src/commonTest/kotlin/co/touchlab/sessionize/EventModelTest.kt
+++ sessionize/lib/src/commonTest/kotlin/co/touchlab/sessionize/EventModelTest.kt
@@ -2,6 +2,7 @@ package co.touchlab.sessionize
 
 import co.touchlab.sessionize.api.AnalyticsApi
 import co.touchlab.sessionize.api.SessionizeApi
+import co.touchlab.sessionize.api.NotificationsApi
 import co.touchlab.sessionize.platform.TestConcurrent
 import kotlinx.coroutines.Dispatchers
 import kotlin.test.BeforeTest
@@ -12,11 +13,12 @@ import kotlin.test.assertTrue
 class EventModelTest {
     private val sessionizeApiMock = SessionizeApiMock()
     private val analyticsApiMock = AnalyticsApiMock()
+    private val notificationsApiMock = NotificationsApiMock()
 
     @BeforeTest
     fun setup() {
         ServiceRegistry.initServiceRegistry(testDbConnection(),
-                Dispatchers.Main, TestSettings(), TestConcurrent, SessionizeApiMock(), AnalyticsApiMock(), "-0400")
+                Dispatchers.Main, TestSettings(), TestConcurrent, SessionizeApiMock(), AnalyticsApiMock(), NotificationsApiMock(), "-0400")
 
         AppContext.initAppContext({filePrefix, fileType ->
             when(filePrefix){
@@ -35,9 +37,9 @@ class EventModelTest {
         val eventModel = EventModel("67316")
         val session = AppContext.sessionQueries.sessionById("67316").executeAsOne()
         val si = collectSessionInfo(session)
-        /*eventModel.toggleRsvpSuspend(si)
+        eventModel.toggleRsvpSuspend(si)
         assertTrue { sessionizeApiMock.rsvpCalled }
-        assertTrue { analyticsApiMock.logCalled }*/
+        assertTrue { analyticsApiMock.logCalled }
     }
 }
 
@@ -68,4 +70,24 @@ class SessionizeApiMock : SessionizeApi {
         rsvpCalled = true
         return true
     }
+}
+
+class NotificationsApiMock : NotificationsApi {
+
+    override fun createLocalNotification(title:String, message:String, timeInMS:Long, notificationId: Int){
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun cancelLocalNotification(notificationId: Int){
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initializeNotifications(){
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun deinitializeNotifications(){
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
