61df07cb479825d2902c8bae3801aeba4b29e232
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2019-04-12 12:57:23 -0400

    Added images to ios version and added tinting

--- iosApp/iosApp/Feedback/FeedbackRatingSubView.swift
+++ iosApp/iosApp/Feedback/FeedbackRatingSubView.swift
@@ -12,21 +12,43 @@ class FeedbackRatingSubView: UIView {
     @IBOutlet weak var selectionTitle: UILabel!
     @IBOutlet weak var addCommentButton: UIButton!
     
+    @IBOutlet weak var goodButton: UIButton!
+    @IBOutlet weak var okButton: UIButton!
+    @IBOutlet weak var badButton: UIButton!
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        
+        let tintableImageGood = goodButton.currentBackgroundImage?.withRenderingMode(.alwaysTemplate)
+        goodButton.setBackgroundImage(tintableImageGood,for: UIControlState.normal)
+        
+        let tintableImageOk = okButton.currentBackgroundImage?.withRenderingMode(.alwaysTemplate)
+        okButton.setBackgroundImage(tintableImageOk,for: UIControlState.normal)
+        
+        let tintableImageBad = badButton.currentBackgroundImage?.withRenderingMode(.alwaysTemplate)
+        badButton.setBackgroundImage(tintableImageBad,for: UIControlState.normal)
     }
     
     private var feedbackHandler: FeedbackInteractionDelegate?
     
     @IBAction func goodButtonPressed(_ sender: Any) {
+        unhighlightButtons()
+        goodButton.tintColor = UIColor.blue
         buttonPressed(rating: FeedbackRating.good)
     }
     
     @IBAction func okButtonPressed(_ sender: Any) {
+        unhighlightButtons()
+        okButton.tintColor = UIColor.blue
         buttonPressed(rating: FeedbackRating.ok)
     }
     
     @IBAction func badButtonPressed(_ sender: Any) {
+        unhighlightButtons()
+        badButton.tintColor = UIColor.blue
         buttonPressed(rating: FeedbackRating.bad)
     }
     
@@ -48,7 +70,13 @@ class FeedbackRatingSubView: UIView {
     }
     
     private func activateCommentButton(){
-        addCommentButton.titleLabel?.textColor = UIColor.darkGray
+        addCommentButton.setTitleColor(UIColor.darkGray, for: .normal)
         addCommentButton.isEnabled = true
+    }
+    
+    private func unhighlightButtons(){
+        goodButton.tintColor = UIColor.black
+        okButton.tintColor = UIColor.black
+        badButton.tintColor = UIColor.black
     }
 }
