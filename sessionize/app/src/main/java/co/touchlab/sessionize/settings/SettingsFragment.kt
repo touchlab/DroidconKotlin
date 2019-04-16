aa0bf01ff347a451c5773a5a33117b94e7245fa2
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2019-04-16 16:50:12 -0400

    Added settings adapter with custom ViewHolders

--- sessionize/app/src/main/java/co/touchlab/sessionize/settings/SettingsFragment.kt
+++ sessionize/app/src/main/java/co/touchlab/sessionize/settings/SettingsFragment.kt
@@ -5,9 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import co.touchlab.sessionize.R
 
 class SettingsFragment : Fragment() {
+
+    lateinit var recycler: RecyclerView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +22,28 @@ class SettingsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+        recycler = view.findViewById(R.id.recycler)
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recycler = view.findViewById<RecyclerView>(R.id.recycler)
+        recycler.layoutManager = LinearLayoutManager(getActivity())
+
+        updateContent()
+    }
+
+    private fun updateContent() {
+        val adapter = SettingsAdapter(activity!!)
+
+        adapter.addSwitchRow("Enable Feedback",R.drawable.icon_twitter,true)
+        adapter.addTextRow("About Us",R.drawable.icon_company)
+
+        recycler.adapter = adapter
     }
 
 
