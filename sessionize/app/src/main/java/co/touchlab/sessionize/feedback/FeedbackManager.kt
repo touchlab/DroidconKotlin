c0d3ce3497fee49066761ec9baf3ad6dd8918a48
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2019-04-17 17:42:05 -0400

    Moved feedback functionality to FeedbackModel and added test

--- sessionize/app/src/main/java/co/touchlab/sessionize/feedback/FeedbackManager.kt
+++ sessionize/app/src/main/java/co/touchlab/sessionize/feedback/FeedbackManager.kt
@@ -2,63 +2,36 @@ package co.touchlab.sessionize.feedback
 
 import androidx.fragment.app.FragmentManager
 import co.touchlab.droidcon.db.MySessions
-import co.touchlab.sessionize.AppContext
-import co.touchlab.sessionize.AppContext.SessionListener
-import co.touchlab.sessionize.platform.NotificationFeedbackTag
-import co.touchlab.sessionize.platform.cancelLocalNotification
+import co.touchlab.sessionize.FeedbackModel
+import co.touchlab.sessionize.api.FeedbackApi
 
-class FeedbackManager : SessionListener{
+class FeedbackManager : FeedbackApi {
 
 
     private var fragmentManager:FragmentManager? = null
+    private var feedbackModel:FeedbackModel = FeedbackModel()
 
-    private var sessionIdx:Int = 0
-    private var sessions: List<MySessions>? = null
 
     fun setFragmentManager(fragmentManager: FragmentManager){
         this.fragmentManager = fragmentManager
     }
 
     fun showFeedbackForPastSessions(){
-        AppContext.requestMySessionsForFeedback(this)
+        feedbackModel.showFeedbackForPastSessions(this)
     }
 
-    private fun generateFeedbackDialog(session: MySessions){
+    override fun generateFeedbackDialog(session: MySessions){
         val feedbackDialog = FeedbackDialog()
         feedbackDialog.showNow(fragmentManager, "FeedbackDialog")
         feedbackDialog.setSessionInfo(session.id, session.title)
         feedbackDialog.setFeedbackManager(this)
     }
 
-    private fun getNextSessionFromList(): MySessions?{
-        sessionIdx++
-        sessions?.let {
-            if(sessionIdx < it.count()) {
-                return it[sessionIdx]
-            }
-        }
-        return null
+    fun finishedFeedback(sessionId:String, rating:Int, comment: String) {
+        feedbackModel.finishedFeedback(sessionId,rating,comment)
     }
 
-    fun finishedFeedback(sessionId:String, rating:Int, comment: String){
-        AppContext.setSessionFeedback(sessionId,rating,comment, this)
-    }
-
-    override fun onMySessionsRetrieved(sessions: List<MySessions>) {
-        if(sessions.isNotEmpty()) {
-            sessionIdx = 0
-            this.sessions = sessions
-            this.sessions?.let {
-                generateFeedbackDialog(it[sessionIdx])
-            }
-        }
-    }
-
-    override fun onFeedbackSaved(sessionId: String) {
-        cancelLocalNotification(sessionId.hashCode(), NotificationFeedbackTag)
-
-        getNextSessionFromList()?.let {
-            generateFeedbackDialog(it)
-        }
+    override fun onError(error: FeedbackApi.FeedBackError) {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
