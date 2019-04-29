3adf0e12e4570d8ac85bab8a8fc1f636d7a1a473
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2019-04-29 14:15:43 -0400

    added imports and updated settings model calls

--- sessionize/lib/src/commonMain/kotlin/co/touchlab/sessionize/AboutModel.kt
+++ sessionize/lib/src/commonMain/kotlin/co/touchlab/sessionize/AboutModel.kt
@@ -1,5 +1,6 @@
 package co.touchlab.sessionize
 
+import co.touchlab.sessionize.AppContext.clLog
 import co.touchlab.sessionize.platform.backgroundSuspend
 import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
