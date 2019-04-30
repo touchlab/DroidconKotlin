b2c9f58a9e0782326f566cf606ea72bb67733181
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2019-04-30 16:04:04 -0400

    Added feedback enabled into the feedback Management

--- sessionize/lib/src/commonMain/kotlin/co/touchlab/sessionize/AppContext.kt
+++ sessionize/lib/src/commonMain/kotlin/co/touchlab/sessionize/AppContext.kt
@@ -1,27 +1,22 @@
 package co.touchlab.sessionize
 
-import co.touchlab.droidcon.db.MySessions
 import co.touchlab.droidcon.db.RoomQueries
 import co.touchlab.droidcon.db.SessionQueries
 import co.touchlab.droidcon.db.SponsorQueries
 import co.touchlab.droidcon.db.UserAccountQueries
-import co.touchlab.sessionize.api.NotificationsApi
 import co.touchlab.sessionize.api.notificationFeedbackTag
 import co.touchlab.sessionize.api.notificationReminderTag
 import co.touchlab.sessionize.db.SessionizeDbHelper
-import co.touchlab.sessionize.platform.Date
 import co.touchlab.sessionize.platform.backgroundSuspend
 import co.touchlab.sessionize.platform.backgroundTask
 import co.touchlab.sessionize.platform.createUuid
 import co.touchlab.sessionize.platform.currentTimeMillis
+import co.touchlab.sessionize.platform.feedbackEnabled
 import co.touchlab.sessionize.platform.logException
+import co.touchlab.sessionize.platform.notificationsEnabled
 import co.touchlab.sessionize.platform.reminderNotificationsEnabled
-import co.touchlab.stately.concurrency.AtomicReference
 import co.touchlab.stately.concurrency.ThreadLocalRef
 import co.touchlab.stately.concurrency.value
-import co.touchlab.stately.freeze
-import com.russhwolf.settings.get
-import com.russhwolf.settings.set
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import kotlin.coroutines.CoroutineContext
@@ -154,27 +149,32 @@ object AppContext {
     }
 
     private fun createNotificationsForSessions() {
-        if (reminderNotificationsEnabled()) {
+        if(notificationsEnabled() && (reminderNotificationsEnabled() || feedbackEnabled())) {
             backgroundTask({ sessionQueries.mySessions().executeAsList() }) { mySessions ->
-                mySessions.forEach { session ->
-                    val notificationTime = session.startsAt.toLongMillis() - TEN_MINS_MILLIS
-                    if (notificationTime > currentTimeMillis()) {
-                        ServiceRegistry.notificationsApi.createLocalNotification("Upcoming Event in " + session.roomName,
-                                session.title + " is starting soon.",
-                                notificationTime,
-                                session.id.hashCode(),
-                                notificationReminderTag)
+                if (reminderNotificationsEnabled()) {
+                    mySessions.forEach { session ->
+                        val notificationTime = session.startsAt.toLongMillis() - TEN_MINS_MILLIS
+                        if (notificationTime > currentTimeMillis()) {
+                            ServiceRegistry.notificationsApi.createLocalNotification("Upcoming Event in " + session.roomName,
+                                    session.title + " is starting soon.",
+                                    notificationTime,
+                                    session.id.hashCode(),
+                                    notificationReminderTag)
+                        }
                     }
-
-                    // Feedback Notifications
-                    if (session.feedbackRating == null) {
-                        val feedbackNotificationTime = session.endsAt.toLongMillis() + TEN_MINS_MILLIS
-                        ServiceRegistry.notificationsApi.createLocalNotification("How was the session?",
-                                " Leave feedback for " + session.title,
-                                feedbackNotificationTime,
-                                //Not great. Possible to clash, although super unlikely
-                                session.id.hashCode(),
-                                notificationFeedbackTag)
+                }
+                // Feedback Notifications
+                if (feedbackEnabled()) {
+                    mySessions.forEach { session ->
+                        if(session.feedbackRating == null) {
+                            val feedbackNotificationTime = session.endsAt.toLongMillis() + TEN_MINS_MILLIS
+                            ServiceRegistry.notificationsApi.createLocalNotification("How was the session?",
+                                    " Leave feedback for " + session.title,
+                                    feedbackNotificationTime,
+                                    //Not great. Possible to clash, although super unlikely
+                                    session.id.hashCode(),
+                                    notificationFeedbackTag)
+                        }
                     }
                 }
             }
