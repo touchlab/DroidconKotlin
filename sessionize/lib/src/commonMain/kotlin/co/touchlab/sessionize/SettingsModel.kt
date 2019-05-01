e666f8169858d416d4145de30278dc9efbc3c0f4
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2019-05-01 11:32:41 -0400

    Updated notifications Api to handle specific notifications

--- sessionize/lib/src/commonMain/kotlin/co/touchlab/sessionize/SettingsModel.kt
+++ sessionize/lib/src/commonMain/kotlin/co/touchlab/sessionize/SettingsModel.kt
@@ -2,25 +2,64 @@ package co.touchlab.sessionize
 
 import co.touchlab.sessionize.AppContext.FEEDBACK_ENABLED
 import co.touchlab.sessionize.AppContext.REMINDERS_ENABLED
+import co.touchlab.sessionize.AppContext.sessionQueries
+import co.touchlab.sessionize.platform.backgroundTask
+import co.touchlab.sessionize.platform.feedbackEnabled
+import co.touchlab.sessionize.platform.notificationsEnabled
+import co.touchlab.sessionize.platform.reminderNotificationsEnabled
 import com.russhwolf.settings.set
 import kotlinx.coroutines.launch
 
 
 class SettingsModel : BaseModel(ServiceRegistry.coroutinesDispatcher) {
 
-    fun setFeedbackEnabled(enabled:Boolean) = launch {
-        if(enabled){
-            ServiceRegistry.notificationsApi.initializeNotifications()
-        }
+    fun setRemindersEnabled(enabled:Boolean) = launch {
+        ServiceRegistry.appSettings[REMINDERS_ENABLED] = enabled
 
-        ServiceRegistry.appSettings[FEEDBACK_ENABLED] = enabled
+        if(enabled && !notificationsEnabled()){
+            ServiceRegistry.notificationsApi.initializeNotifications{success ->
+                if(success) {
+                    handleReminderNotifications(enabled)
+                    handleFeedbackNotifications(feedbackEnabled())
+                }
+            }
+        }else{
+            handleFeedbackNotifications(enabled)
+        }
     }
 
-    fun setRemindersEnabled(enabled:Boolean) = launch {
-        if(enabled){
-            ServiceRegistry.notificationsApi.initializeNotifications()
-        }
+    fun setFeedbackEnabled(enabled:Boolean) = launch {
+        ServiceRegistry.appSettings[FEEDBACK_ENABLED] = enabled
 
-        ServiceRegistry.appSettings[REMINDERS_ENABLED] = enabled
+        if(enabled && !notificationsEnabled()){
+            ServiceRegistry.notificationsApi.initializeNotifications{success ->
+                if(success) {
+                    handleFeedbackNotifications(enabled)
+                    handleReminderNotifications(reminderNotificationsEnabled())
+                }
+            }
+        }else{
+            handleFeedbackNotifications(enabled)
+        }
+    }
+
+    private fun handleReminderNotifications(create:Boolean){
+        backgroundTask({ sessionQueries.mySessions().executeAsList() }) { mySessions ->
+            if(create){
+                ServiceRegistry.notificationsApi.createReminderNotificationsForSessions(mySessions)
+            }else{
+                ServiceRegistry.notificationsApi.cancelReminderNotificationsForSessions(mySessions)
+            }
+        }
+    }
+
+    private fun handleFeedbackNotifications(create:Boolean){
+        backgroundTask({ sessionQueries.mySessions().executeAsList() }) { mySessions ->
+            if(create){
+                ServiceRegistry.notificationsApi.createFeedbackNotificationsForSessions(mySessions)
+            }else {
+                ServiceRegistry.notificationsApi.cancelFeedbackNotificationsForSessions(mySessions)
+            }
+        }
     }
 }
--- sessionize/lib/src/commonMain/kotlin/co/touchlab/sessionize/api/NotificationsApi.kt
+++ sessionize/lib/src/commonMain/kotlin/co/touchlab/sessionize/api/NotificationsApi.kt
@@ -1,15 +1,28 @@
 package co.touchlab.sessionize.api
 
+import co.touchlab.droidcon.db.MySessions
+
 const val notificationFeedbackTag = "Feedback"
 const val notificationReminderTag = "Reminder"
 
 interface NotificationsApi {
 
+    fun createReminderNotificationsForSessions(sessions:List<MySessions>)
+
+    fun createFeedbackNotificationsForSessions(sessions:List<MySessions>)
+
+    fun cancelReminderNotificationsForSessions(sessions:List<MySessions>)
+
+    fun cancelFeedbackNotificationsForSessions(sessions:List<MySessions>)
+
+
+    // General Notification Code
+
     fun createLocalNotification(title:String, message:String, timeInMS:Long, notificationId: Int, notificationTag: String)
 
     fun cancelLocalNotification(notificationId: Int, notificationTag: String)
 
-    fun initializeNotifications()
+    fun initializeNotifications(onSuccess: (Boolean) -> Unit)
 
     fun deinitializeNotifications()
 
