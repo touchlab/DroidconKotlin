6b85904e1c8c986786e261ad2083ecad74a7c194
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2019-05-08 12:36:21 -0400

    removed unused parenthesis

--- sessionize/lib/src/commonMain/kotlin/co/touchlab/sessionize/db/DateAdapter.kt
+++ sessionize/lib/src/commonMain/kotlin/co/touchlab/sessionize/db/DateAdapter.kt
@@ -6,7 +6,7 @@ import com.squareup.sqldelight.ColumnAdapter
 
 const val SESSIONIZE_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ"
 
-class DateAdapter() : ColumnAdapter<Date, String> {
+class DateAdapter : ColumnAdapter<Date, String> {
     val formatter = DateFormatHelper(SESSIONIZE_DATE_FORMAT)
     override fun encode(value: Date) = formatter.formatConferenceTZ(value)
     override fun decode(databaseValue: String) = formatter.toConferenceDate(databaseValue)
