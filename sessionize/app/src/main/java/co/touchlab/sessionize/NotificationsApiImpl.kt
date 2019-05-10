d51989e57113d5e60bca0edf48fc9813bc851735
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2019-05-10 09:48:01 -0400

    Replaced AlarmManager with WorkManager

--- sessionize/app/src/main/java/co/touchlab/sessionize/NotificationsApiImpl.kt
+++ sessionize/app/src/main/java/co/touchlab/sessionize/NotificationsApiImpl.kt
@@ -1,55 +1,50 @@
 package co.touchlab.sessionize
 
-import android.app.AlarmManager
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.BroadcastReceiver
-import android.content.ComponentName
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.Worker
+import androidx.work.WorkerParameters
 import app.sessionize.touchlab.lib.R
 import co.touchlab.sessionize.api.NotificationsApi
 import co.touchlab.sessionize.platform.AndroidAppContext
 import co.touchlab.sessionize.platform.NotificationsModel.setNotificationsEnabled
+import co.touchlab.sessionize.platform.currentTimeMillis
+import java.util.concurrent.TimeUnit
 
+
+private const val keyTitle = "title"
+private const val keyMessage = "message"
+private const val keyTimeinMs = "timeinMS"
+private const val keyNotificationId = "notification_id"
+private const val keyNotificationTag = "notification_tag"
 
 class NotificationsApiImpl : NotificationsApi {
 
-    private val notificationPublisher: BroadcastReceiver = NotificationPublisher()
-
     override fun createLocalNotification(title:String, message:String, timeInMS:Long, notificationId: Int, notificationTag: String) {
-        // Building Notification
-        val channelId = AndroidAppContext.app.getString(R.string.notification_channel_id)
-        val builder = NotificationCompat.Builder(AndroidAppContext.app, channelId)
-                .setSmallIcon(R.drawable.baseline_insert_invitation_24)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setWhen(timeInMS)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setCategory(NotificationCompat.CATEGORY_REMINDER)
 
+        val data = Data.Builder()
+        data.put(keyTitle,title)
+        data.put(keyMessage, message)
+        data.put(keyTimeinMs, timeInMS)
+        data.put(keyNotificationId,notificationId)
+        data.put(keyNotificationTag, notificationTag)
 
-        // Building Intent wrapper
-        val intent = Intent().also { intent ->
-            intent.action = AndroidAppContext.app.getString(R.string.notification_action)
-            intent.putExtra(NotificationPublisher.NOTIFICATION_ID, notificationId)
-            intent.putExtra(NotificationPublisher.NOTIFICATION_TAG, notificationTag)
-            intent.putExtra(NotificationPublisher.NOTIFICATION, builder.build())
-            val componentName = ComponentName(AndroidAppContext.app, NotificationPublisher::class.java)
-            intent.component = componentName
-        }
+        val delay = timeInMS - currentTimeMillis()
+        val notificationWorkRequest = OneTimeWorkRequestBuilder<NotificationWorker>()
+                .setInputData(data.build())
+                .addTag(notificationTag + notificationId.toString())
+                .setInitialDelay(delay, TimeUnit.MILLISECONDS)
+                .build()
 
-        val pendingIntent = PendingIntent.getBroadcast(AndroidAppContext.app, notificationId, intent, PendingIntent.FLAG_CANCEL_CURRENT)
-
-        // Scheduling Intent
-        val alarmManager = AndroidAppContext.app.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        alarmManager.set(AlarmManager.RTC_WAKEUP, timeInMS, pendingIntent)
+        WorkManager.getInstance().cancelAllWorkByTag(notificationTag + notificationId.toString())
+        WorkManager.getInstance().enqueue(notificationWorkRequest)
     }
 
     override fun cancelLocalNotification(notificationId: Int, notificationTag: String) {
@@ -63,35 +58,12 @@ class NotificationsApiImpl : NotificationsApi {
 
     override fun initializeNotifications(onSuccess: (Boolean) -> Unit)
     {
-        val filter = IntentFilter(AndroidAppContext.app.getString(R.string.notification_action))
-        AndroidAppContext.app.registerReceiver(notificationPublisher, filter)
-
         createNotificationChannel()
         setNotificationsEnabled(true)
         onSuccess(true)
     }
 
     override fun deinitializeNotifications() {
-    }
-
-    class NotificationPublisher : BroadcastReceiver() {
-
-        override fun onReceive(context: Context, intent: Intent) {
-            val notification = intent.getParcelableExtra<Notification>(NOTIFICATION)
-            val notificationId = intent.getIntExtra(NOTIFICATION_ID, 0)
-            val notificationTag = intent.getStringExtra(NOTIFICATION_TAG)
-
-            with(NotificationManagerCompat.from(AndroidAppContext.app)) {
-                // notificationId is a unique int for each notification that you must define
-                this.notify(notificationTag, notificationId, notification)
-            }
-        }
-
-        companion object {
-            var NOTIFICATION_ID = "notification_id"
-            var NOTIFICATION_TAG = "notification_tag"
-            var NOTIFICATION = "notification"
-        }
     }
 
     private fun createNotificationChannel() {
@@ -115,4 +87,41 @@ class NotificationsApiImpl : NotificationsApi {
             }
         }
     }
+
+    class NotificationWorker(appContext: Context, workerParams: WorkerParameters)
+        : Worker(appContext, workerParams) {
+
+        override fun doWork(): Result {
+
+            val title = inputData.getString(keyTitle)
+            val message = inputData.getString(keyMessage)
+            val timeInMS = inputData.getLong(keyTimeinMs,-1)
+            val notificationId = inputData.getInt(keyNotificationId, -1)
+            val notificationTag = inputData.getString(keyNotificationTag)
+
+            if(title.isNullOrBlank() || message.isNullOrBlank() || notificationTag.isNullOrBlank()){
+                return Result.failure()
+            }
+            if(timeInMS == -1L || notificationId == -1){
+                return Result.failure()
+            }
+
+
+            val channelId = AndroidAppContext.app.getString(R.string.notification_channel_id)
+            val builder = NotificationCompat.Builder(AndroidAppContext.app, channelId)
+                    .setSmallIcon(R.drawable.baseline_insert_invitation_24)
+                    .setContentTitle(title)
+                    .setContentText(message)
+                    .setWhen(timeInMS)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setCategory(NotificationCompat.CATEGORY_REMINDER)
+
+
+            with(NotificationManagerCompat.from(AndroidAppContext.app)) {
+                this.notify(notificationTag, notificationId, builder.build())
+            }
+            return Result.success()
+        }
+    }
+
 }
