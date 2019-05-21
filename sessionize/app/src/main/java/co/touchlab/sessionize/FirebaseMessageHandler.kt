74aaf8bbb720fe689653e0593086a7cc3c8aa2a5
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2019-05-21 12:24:00 -0400

    Moved Firebase message handler to app instead of lib, to match the iOS version

---
+++ sessionize/app/src/main/java/co/touchlab/sessionize/FirebaseMessageHandler.kt
@@ -0,0 +1,44 @@
+package co.touchlab.sessionize
+
+import android.content.Context
+import android.util.Log
+import co.touchlab.sessionize.api.NetworkRepo
+import com.google.firebase.FirebaseApp
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class FirebaseMessageHandler : FirebaseMessagingService() {
+
+    private var token:String? = null
+
+    fun requestToken(){
+        FirebaseInstanceId.getInstance().instanceId.addOnCompleteListener OnCompleteListener@{ task ->
+            if (!task.isSuccessful) {
+                Log.w("TAG", "getInstanceId failed", task.exception)
+                return@OnCompleteListener
+            }
+            token = task.result?.token
+        }
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        Log.d(TAG, "From: ${remoteMessage?.from}")
+
+        remoteMessage?.data?.isNotEmpty()?.let {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+        }
+
+        NetworkRepo.dataCalls()
+    }
+    override fun onNewToken(token: String?) {
+        this.token = token
+    }
+
+    companion object {
+        val TAG = FirebaseMessageHandler.javaClass.simpleName
+        fun initFirebaseApp(context: Context){
+            FirebaseApp.initializeApp(context)
+        }
+    }
+}
