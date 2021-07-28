48eccbd94310787e24b45e8d5f5d718bd4982930
Julia Jakubcova <julia@brightify.org>
2021-07-28 09:52:28 +0200

    Set theme colors, prepare toolbar.

--- android/src/main/java/co/touchlab/droidcon/android/ui/theme/Color.kt
+++ android/src/main/java/co/touchlab/droidcon/android/ui/theme/Color.kt
@@ -2,7 +2,16 @@ package co.touchlab.droidcon.android.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+object Colors {
+
+    val teal = Color(0xFF14A1B6)
+    val darkBlue = Color(0xFF0E65B1)
+    val lightYellow = Color(0xFFFED050)
+    val skyBlue = Color(0xFF68d1fd)
+    val orange = Color(0xFFf1582c)
+
+    val darkGrey = Color(0xFF333333)
+    val lightGrey220 = Color(0xFFdcdcdc)
+    val lightGrey222 = Color(0xFFDEDEDE)
+    val lightGrey250 = Color(0xFFfafafa)
+}
--- android/src/main/java/co/touchlab/droidcon/android/ui/theme/Theme.kt
+++ android/src/main/java/co/touchlab/droidcon/android/ui/theme/Theme.kt
@@ -5,26 +5,32 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Colors.teal,
+    primaryVariant = Colors.darkBlue,
+    secondary = Colors.lightYellow,
+    surface = Color.White,
+    background = Color.White,
+    error = Colors.orange,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onSurface = Color.Black,
+    onBackground = Color.Black,
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
+    primary = Colors.teal,
+    primaryVariant = Colors.darkBlue,
+    secondary = Colors.lightYellow,
     surface = Color.White,
+    background = Color.White,
+    error = Colors.orange,
     onPrimary = Color.White,
     onSecondary = Color.Black,
-    onBackground = Color.Black,
     onSurface = Color.Black,
-    */
+    onBackground = Color.Black,
 )
 
 @Composable
