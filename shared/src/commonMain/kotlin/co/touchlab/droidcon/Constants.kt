51218e1e86cbfb5919905548753907a38aa5f872
Kevin Galligan <kgalligan@gmail.com>
2021-09-28 20:40:52 -0400

    Data for Berlin 2021

--- shared/src/commonMain/kotlin/co/touchlab/droidcon/Constants.kt
+++ shared/src/commonMain/kotlin/co/touchlab/droidcon/Constants.kt
@@ -15,7 +15,7 @@ object Constants {
     }
 
     object Sessionize {
-        const val scheduleId = "jmuc9diq"
-        const val sponsorsId = "jmuc9diq"
+        const val scheduleId = "2wi6ppp2"
+        const val sponsorsId = "2wi6ppp2"
     }
 }
