acb54618d363e26a1c7e025d032576c4084c9f25
Tadeas Kriz <tadeas@brightify.org>
2021-10-18 23:20:57 +0200

    Use session end correctly.

--- android/src/main/java/co/touchlab/droidcon/android/viewModel/sessions/SessionsBlockViewModel.kt
+++ android/src/main/java/co/touchlab/droidcon/android/viewModel/sessions/SessionsBlockViewModel.kt
@@ -3,6 +3,7 @@ package co.touchlab.droidcon.android.viewModel.sessions
 import androidx.lifecycle.ViewModel
 import co.touchlab.droidcon.android.service.DateTimeFormatterViewService
 import co.touchlab.droidcon.domain.composite.ScheduleItem
+import co.touchlab.droidcon.domain.service.DateTimeService
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -13,18 +14,20 @@ import org.koin.core.component.inject
 
 class SessionsBlockViewModel(
     startsAt: Instant,
+    endsAt: Instant,
     items: List<ScheduleItem>,
 ): ViewModel(), KoinComponent {
 
     private val dateTimeFormatter by inject<DateTimeFormatterViewService>()
 
+    private val dateTimeService by inject<DateTimeService>()
+
     val time: String = dateTimeFormatter.time(startsAt)
     val sessions: List<SessionViewModel> = items.map(::SessionViewModel)
 
-    //TODOKPG - Checking start instant for hasEnded is weird
     val hasEnded: Flow<Boolean> = flow {
         while (true) {
-            val hasEnded = Clock.System.now() > startsAt
+            val hasEnded = dateTimeService.now() > endsAt
             emit(hasEnded)
             delay(1_000)
         }
