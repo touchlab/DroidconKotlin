bf4dbf2a871b03ee99bad3fc41564585aab9a5e2
Julia Jakubcova <j.jakubcova@touchlab.co>
2022-08-22 10:59:56 +0200

    Change title, time zone and bundle id.

--- shared/src/commonMain/kotlin/co/touchlab/droidcon/Constants.kt
+++ shared/src/commonMain/kotlin/co/touchlab/droidcon/Constants.kt
@@ -3,21 +3,21 @@ package co.touchlab.droidcon
 import kotlinx.datetime.TimeZone
 
 object Constants {
-    val conferenceTimeZone = TimeZone.of("Europe/Berlin")
+    val conferenceTimeZone = TimeZone.of("America/New_York")
 
     /**
      * String of `${timeZoneString}|${Sessionize.scheduleId}` run through md5
      * When time zone is changed, generate a new hash by running `md5 -s "${timeZoneString}|${Sessionize.scheduleId}"` in command line
      */
-    val conferenceTimeZoneHash = "b0ff26808102a633934723c03818b085"
+    val conferenceTimeZoneHash = "8b484e09200df61364e7affaff42d261"
 
     object Firestore {
 
         const val projectId = "droidcon-148cc"
         const val databaseName = "(default)"
 
-        // Known variants: "sponsors", "sponsors-lisbon-2019", "sponsors-sf-2019", "sponsors-sf-2022"
-        const val collectionName = "sponsors-berlin-2022"
+        // Known variants: "sponsors", "sponsors-lisbon-2019", "sponsors-sf-2019", "sponsors-sf-2022", "sponsors-berlin-2022"
+        const val collectionName = "sponsors-nyc-2022"
         const val apiKey = "AIzaSyCkD5DH2rUJ8aZuJzANpIFj0AVuCNik1l0"
     }
 
@@ -26,4 +26,3 @@ object Constants {
         const val sponsorsId = "zwd2wtgt"
     }
 }
-
