d8416b1d73a198de3f6ad74dde69283605cbd68d
Kyle Roe <[hopeman15@users.noreply.github.com]>
2022-08-25 22:01:04 +0200

    Clean up session block

--- android/src/main/java/co/touchlab/droidcon/android/ui/sessions/SessionBlock.kt
+++ android/src/main/java/co/touchlab/droidcon/android/ui/sessions/SessionBlock.kt
@@ -1,38 +1,35 @@
 package co.touchlab.droidcon.android.ui.sessions
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.Badge
-import androidx.compose.material.BadgedBox
-import androidx.compose.material.Card
-import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import co.touchlab.droidcon.R
+import co.touchlab.droidcon.android.ui.sessions.components.SessionCard
+import co.touchlab.droidcon.android.ui.sessions.components.SessionView
+import co.touchlab.droidcon.android.ui.theme.Colors
 import co.touchlab.droidcon.android.ui.theme.Dimensions
 import co.touchlab.droidcon.android.viewModel.sessions.SessionViewModel
 import co.touchlab.droidcon.android.viewModel.sessions.SessionsBlockViewModel
 import co.touchlab.droidcon.ui.theme.Colors
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun SessionBlock(sessionsBlock: SessionsBlockViewModel, attendingOnly: Boolean, sessionTapped: (SessionViewModel) -> Unit) {
-    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.TopStart) {
+fun SessionBlock(
+    sessionsBlock: SessionsBlockViewModel,
+    attendingOnly: Boolean,
+    sessionTapped: (SessionViewModel) -> Unit,
+) {
+    Row(modifier = Modifier.fillMaxWidth()) {
         Text(
             text = sessionsBlock.time,
             modifier = Modifier
@@ -42,64 +39,33 @@ fun SessionBlock(sessionsBlock: SessionsBlockViewModel, attendingOnly: Boolean, 
         )
 
         val hasEnded by sessionsBlock.hasEnded.collectAsState(initial = false)
+        val backgroundColor = if (hasEnded) MaterialTheme.colors.surface else MaterialTheme.colors.background
+        val border = if (MaterialTheme.colors.isLight || hasEnded) null else BorderStroke(1.dp, MaterialTheme.colors.surface)
 
-        Column(modifier = Modifier.padding(start = 72.dp)) {
+        SessionCard(
+            backgroundColor = backgroundColor,
+            border = border,
+        ) {
             sessionsBlock.sessions.forEach { session ->
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    val badgeColor = when {
-                        attendingOnly || !session.isAttending -> Color.Transparent
-                        hasEnded -> Colors.grey
-                        session.isColliding -> Colors.orange
-                        else -> Colors.skyBlue
-                    }
-                    BadgedBox(
-                        modifier = Modifier.padding(Dimensions.Padding.default),
-                        badge = { Badge(backgroundColor = badgeColor) { } }
-                    ) { }
-
-                    val backgroundColor = if (hasEnded) MaterialTheme.colors.surface else MaterialTheme.colors.background
-                    val isClickable = !session.isServiceSession
-                    Card(
-                        modifier = Modifier.weight(1f),
-                        backgroundColor = backgroundColor,
-                        onClick = {
-                            sessionTapped(session)
-                        },
-                        elevation = 2.dp,
-                        enabled = isClickable,
-                        border = if (MaterialTheme.colors.isLight || hasEnded) null else BorderStroke(1.dp, MaterialTheme.colors.surface),
-                    ) {
-                        Column {
-                            Text(
-                                text = session.title,
-                                modifier = Modifier.padding(Dimensions.Padding.half),
-                                fontWeight = FontWeight.Bold,
-                            )
-                            session.room?.let { roomName ->
-                                Text(
-                                    text = stringResource(id = R.string.schedule_item_in_room).format(roomName),
-                                    modifier = Modifier.padding(
-                                        start = Dimensions.Padding.half,
-                                        bottom = Dimensions.Padding.half,
-                                    ),
-                                    style = MaterialTheme.typography.caption,
-                                    color = MaterialTheme.colors.onSurface.copy(alpha = 0.8f),
-                                )
-                            }
-                            if (!session.isServiceSession && session.speakers.isNotEmpty()) {
-                                Text(
-                                    text = session.speakers,
-                                    modifier = Modifier.padding(
-                                        start = Dimensions.Padding.half,
-                                        end = Dimensions.Padding.half,
-                                        bottom = Dimensions.Padding.half,
-                                    ),
-                                )
-                            }
-                        }
-                    }
-                }
+                SessionView(
+                    title = session.title,
+                    speakers = session.speakers,
+                    onClick = { sessionTapped(session) },
+                    enabled = !session.isServiceSession,
+                    badgeColor = session.badgeColor(
+                        attendingOnly = attendingOnly,
+                        hasEnded = hasEnded
+                    )
+                )
+                Divider()
             }
         }
     }
+}
+
+private fun SessionViewModel.badgeColor(attendingOnly: Boolean, hasEnded: Boolean) = when {
+    attendingOnly || !isAttending -> Color.Transparent
+    hasEnded -> Colors.grey
+    isColliding -> Colors.orange
+    else -> Colors.skyBlue
 }
