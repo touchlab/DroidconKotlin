d51e7d4c6546ede8ff5e4b776a5c45a4c7f0124d
Balázs Varga <balazs.varga@apter.tech>
2023-05-14 23:29:03 +0200

    Add missing import statement to SessionBlockView.kt

--- shared-ui/src/commonMain/kotlin/co.touchlab.droidcon/ui/session/SessionBlockView.kt
+++ shared-ui/src/commonMain/kotlin/co.touchlab.droidcon/ui/session/SessionBlockView.kt
@@ -2,6 +2,7 @@ package co.touchlab.droidcon.ui.session
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
