2df01999595688180f2b960c1d9b0ebc9ce36d5c
Tadeas Kriz <tadeas@touchlab.co>
2023-09-01 10:21:38 -0400

    Upgrade to Compose Multiplatform 1.5.0.

--- shared-ui/src/iosMain/kotlin/co/touchlab/droidcon/ui/ComposeRootController.kt
+++ shared-ui/src/iosMain/kotlin/co/touchlab/droidcon/ui/ComposeRootController.kt
@@ -1,8 +1,8 @@
 package co.touchlab.droidcon.ui
 
-import androidx.compose.ui.window.Application
+import androidx.compose.ui.window.ComposeUIViewController
 import co.touchlab.droidcon.viewmodel.ApplicationViewModel
 
-fun getRootController(viewModel: ApplicationViewModel) = Application("MainComposeView") {
+fun getRootController(viewModel: ApplicationViewModel) = ComposeUIViewController {
     MainComposeView(viewModel)
 }
