bde577360890898e27cb249f92924bb09cbee1e8
Jigar Brahmbhatt <findjigar@gmail.com>
2024-03-18 15:51:58 -0400

    Remove tests as they're not real and they're causing crashkios + bugsnag test link issue

--- shared/src/commonTest/kotlin/co/touchlab/droidcon/mock/ClockMock.kt
+++
@@ -1,8 +0,0 @@
-package co.touchlab.droidcon.mock
-
-import kotlinx.datetime.Clock
-import kotlinx.datetime.Instant
-
-class ClockMock(var currentInstant: Instant) : Clock {
-    override fun now(): Instant = currentInstant
-}
