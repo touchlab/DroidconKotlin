f89d8d4562355f9dd45af770dbf202a5cfa68ebc
Kevin Schildhorn <kevin.schildhorn@gmail.com>
2024-04-03 11:38:12 -0400

    Adding Compose Support

--- shared-ui/src/iosMain/kotlin/co/touchlab/droidcon/ui/ComposeRootController.kt
+++ shared-ui/src/iosMain/kotlin/co/touchlab/droidcon/ui/ComposeRootController.kt
@@ -1,14 +1,22 @@
 package co.touchlab.droidcon.ui
 
-import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.interop.UIKitViewController
 import androidx.compose.ui.window.ComposeUIViewController
 import co.touchlab.droidcon.viewmodel.ApplicationViewModel
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.UIKit.UIViewController
 
+@OptIn(ExperimentalForeignApi::class)
 fun getRootController(
     viewModel: ApplicationViewModel,
+    createUIViewController: () -> UIViewController,
 ) =
     ComposeUIViewController {
         MainComposeView(viewModel, {
-            Text("This Feature is Currently unsupported in compose multiplatform")
+            UIKitViewController(
+                factory = createUIViewController,
+                modifier = Modifier,
+            )
         })
     }
