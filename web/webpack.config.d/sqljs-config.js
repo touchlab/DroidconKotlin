4e89e9bdfe87d7e962604240b12c6097bfacac2e
Kevin Schildhorn <kevin.schildhorn@gmail.com>
2026-02-12 11:19:08 -0500

    Adding changes for

--- web/webpack.config.d/sqljs-config.js
+++ web/webpack.config.d/sqljs-config.js
@@ -1,16 +1,16 @@
-config.resolve = {
-    fallback: {
-        fs: false,
-        path: false,
-        crypto: false,
-    }
-};
+config.resolve = config.resolve || {};
+config.resolve.fallback = Object.assign(config.resolve.fallback || {}, {
+    fs: false,
+    path: false,
+    crypto: false,
+    os: false,
+});
 
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-config.plugins.push(
-    new CopyWebpackPlugin({
-        patterns: [
-            '../../node_modules/sql.js/dist/sql-wasm.wasm'
-        ]
-    })
-);
+// Serve sql.js WASM from node_modules (avoids needing copy-webpack-plugin)
+const path = require('path');
+if (config.devServer && config.devServer.static) {
+    config.devServer.static.push({
+        directory: path.resolve(__dirname, '../../node_modules/sql.js/dist'),
+        watch: false
+    });
+}
